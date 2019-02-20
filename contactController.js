3a60b023094e21d8d6a977e6d842e9657032b53a
hetu494 <hetansh4@gmail.com>
2019-02-19 21:52:08 -0500

    Minor Changes - Code Cleanup

--- contactController.js
+++ contactController.js
@@ -26,8 +26,8 @@ exports.new = function (req, res) {
     contact.phone = req.body.phone;
 // save the contact and check for errors
     contact.save(function (err) {
-        // if (err)
-        //     res.json(err);
+         if (err)
+             res.json(err);
 res.json({
             message: 'New contact created!',
             data: contact
@@ -45,26 +45,7 @@ exports.view = function (req, res) {
         });
     });
 };
-// Handle update contact info
-/*exports.update = function (req, res) {
-Contact.findById(req.params.contact_id, function (err, contact) {
-        if (err)
-            res.send(err);
-contact.name = req.body.name ? req.body.name : contact.name;
-        contact.gender = req.body.gender;
-        contact.email = req.body.email;
-        contact.phone = req.body.phone;
-// save the contact and check for errors
-        contact.save(function (err) {
-            if (err)
-                res.json(err);
-            res.json({
-                message: 'Contact Info updated',
-                data: contact
-            });
-        });
-    });
-}; */
+
 // Handle delete contact
 exports.delete = function (req, res) {
     Contact.remove({
